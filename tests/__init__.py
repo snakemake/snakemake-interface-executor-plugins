8bae230c67b8b94f85cd76ddcacbb357c7367c9d
Johannes Köster <johannes.koester@uni-due.de>
2023-07-03 10:58:10 +0200

    fix test layout

--- tests/__init__.py
+++
@@ -1,7 +0,0 @@
-from snakemake_executor_plugin_interface import ExecutorPluginRegistry
-from snakemake.executors import AbstractExecutor
-
-def test_registry():
-    # TODO uncomment once the registry implementation is complete
-    # registry = ExecutorPluginRegistry(executor_base_cls=AbstractExecutor)
-    # print(registry.plugins)
