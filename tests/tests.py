f697b1f618ff3d8a272b083504ebd9c371a0acfd
Johannes Köster <johannes.koester@uni-due.de>
2023-07-04 12:44:46 +0200

    add testcases

--- tests/tests.py
+++ tests/tests.py
@@ -15,3 +15,16 @@ def test_registry_register_cli_args():
     registry = ExecutorPluginRegistry(executor_base_cls=AbstractExecutor)
     parser = argparse.ArgumentParser()
     registry.register_cli_args(parser)
+    for action in parser._actions:
+        if not action.dest == "help":
+            assert action.dest.startswith("flux")
+
+
+def test_registry_get_executor_settings():
+    registry = ExecutorPluginRegistry(executor_base_cls=AbstractExecutor)
+    parser = argparse.ArgumentParser()
+    registry.register_cli_args(parser)
+    args = parser.parse_args([])
+    plugin = registry.plugins["flux"]
+    settings = plugin.get_executor_settings(args)
+    assert isinstance(settings, plugin._executor_settings_cls)
