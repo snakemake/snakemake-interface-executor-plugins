f8f7ed50690b92eefeef9bf87f1755298e06cab5
Johannes Köster <johannes.koester@uni-due.de>
2023-07-11 11:42:41 +0200

    fixes

--- snakemake_executor_plugin_interface/executors/__init__.py
+++ snakemake_executor_plugin_interface/executors/__init__.py
@@ -2,10 +2,3 @@ __author__ = "Johannes Köster"
 __copyright__ = "Copyright 2022, Johannes Köster"
 __email__ = "johannes.koester@uni-due.de"
 __license__ = "MIT"
-
-
-from snakemake_executor_plugin_interface.executors import (  # noqa: F401
-    AbstractExecutor,
-    RealExecutor,
-    RemoteExecutor,
-)
