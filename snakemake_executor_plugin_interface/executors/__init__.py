6f2d71e590d33e95660e118fdb1e956130d62ce7
Johannes Köster <johannes.koester@uni-due.de>
2023-07-11 11:06:10 +0200

    reimport

--- snakemake_executor_plugin_interface/executors/__init__.py
+++ snakemake_executor_plugin_interface/executors/__init__.py
@@ -2,3 +2,10 @@ __author__ = "Johannes Köster"
 __copyright__ = "Copyright 2022, Johannes Köster"
 __email__ = "johannes.koester@uni-due.de"
 __license__ = "MIT"
+
+
+from snakemake_executor_plugin_interface.executors import (  # noqa: F401
+    AbstractExecutor,
+    RealExecutor,
+    RemoteExecutor,
+)
