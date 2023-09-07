505bb313c7818cbd10cbd8de2d21b69bb038ed39
Johannes Koester <johannes.koester@uni-due.de>
2023-09-07 21:04:14 +0200

    chore: use cluster-generic as example plugin for tests

--- tests/tests.py
+++ tests/tests.py
@@ -12,7 +12,7 @@ def registry():
 
 def test_registry_collect_plugins(registry):
     assert len(registry.plugins) == 1
-    plugin = registry.plugins["flux"]
+    plugin = registry.plugins["cluster-generic"]
     assert plugin._executor_settings_cls is not None
     assert plugin.common_settings.non_local_exec is True
     assert plugin.executor is not None
@@ -23,14 +23,14 @@ def test_registry_register_cli_args(registry):
     registry.register_cli_args(parser)
     for action in parser._actions:
         if not action.dest == "help":
-            assert action.dest.startswith("flux")
+            assert action.dest.startswith("cluster_generic")
 
 
 def test_registry_get_executor_settings(registry):
     parser = argparse.ArgumentParser()
     registry.register_cli_args(parser)
     args = parser.parse_args([])
-    plugin = registry.plugins["flux"]
+    plugin = registry.plugins["cluster-generic"]
     settings = plugin.get_executor_settings(args)
     print(settings, plugin._executor_settings_cls)
     assert isinstance(settings, plugin._executor_settings_cls)
